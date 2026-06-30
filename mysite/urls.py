7d3417e264335aad2f93f144acff23808a56c64f
Arquemis1 <Frankamaya0226@gmail.com>
2026-06-30 18:19:41 -0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,6 +27,10 @@ urlpatterns = [
     
     # ✅ Rutas de tu aplicación principal (blog)
     path('', include('blog.urls')), 
+       path('accounts/login/', LoginView.as_view(
+        template_name='users/login.html'
+    ), name='login'),
+     path('accounts/', include('django.contrib.auth.urls')),
 
     # ✅ Rutas de cuentas (Login, Registro, Logout)
     # Coincide exactamente con los enlaces que pusimos: /accounts/login/, /accounts/register/
